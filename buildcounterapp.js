e1af3c44c8ffab73d3b916d25809184aa5cc60d2
Shreyansh7sh <shreyanshsharma35062@gmail.com>
2026-01-06 12:48:50 +0530

    Add save functionality and reset count in counter app

--- buildcounterapp.js
+++ buildcounterapp.js
@@ -1,10 +1,13 @@
 let count=0; 
- let peopleEntered = document.getElementById("h2")
+ let peopleEntered = document.getElementById("h2");
+let saveEl = document.getElementById("eneteries");
  function incrementfnct(){
  count = count+1;
  peopleEntered.innerText=count;
 }
 function savefnct(){
-console.log("total number of people entered are : "+ count);
+let str = count + "-";
+saveEl.innerText+=str;
+count=0;
+peopleEntered.innerText=0;
 }
-savefnct();
